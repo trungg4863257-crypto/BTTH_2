8e45eef1b9c753750d6178d7e650a973d4fd7ea9
trungg4863257-crypto <trungg4863257@gmail.com>
2026-05-26 12:23:53 +0700

    Create Ngaythangnam.cpp

---
+++ Ngaythangnam.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef NGAYTHANGNAM_H 
+#define NGAYTHANGNAM_H 
+class NgayThangNam {
+private:
+    int iNgay;
+    int iThang;
+    int iNam;
+public:
+    void Nhap();
+    void Xuat();
+    void NgayThangNamTiepTheo();
+};
+#endif
